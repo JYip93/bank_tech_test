7116a960fafab60094002ca901ab6ff48e78e463
Johnny Yip <jyippy2012@gmail.com>
2020-11-02 12:05:26 +0000

    Added deposit method

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -5,4 +5,12 @@ describe("Account", function(){
             expect(account.balance).toBe(0)
         })
     })
+
+    describe("Deposit money", function(){
+        it("desposit 100 pounds to the account", function(){
+            let account = new Account
+            account.deposit(100)
+            expect(account.balance).toBe(100)
+        })
+    })
 })
