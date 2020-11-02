04be3c0154673c6c32cc8261f2109b335a93a7d2
Johnny Yip <jyippy2012@gmail.com>
2020-11-02 15:16:39 +0000

    Travis CI

---
+++ spec/TransactionListSpec.js
@@ -0,0 +1,10 @@
+const { TransactionList} = require('../src/TransactionList')
+
+
+describe("Transaction List", function(){
+    describe("Get list of transactions back", function(){
+        it("return transaction from transactions list", function(){
+            
+        })
+    })
+})
